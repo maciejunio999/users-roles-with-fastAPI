dbbb68f690d3f0eefe13be1adb8fccc9e76b6476
Maciej Antosz <maciejantosz13@gmail.com>
2025-05-05 22:16:52 +0200

    required to make frontend

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import endpoint, user, role, pilot, authentication, module, menu, product
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 models.Base.metadata.create_all(engine)
